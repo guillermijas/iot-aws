d37fd616f921b0387acc002d2738f046f67bff7f
Guillermo Mora <guillermijas@gmail.com>
2018-01-18 20:15:52 +0100

    Panel de control

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_118_181_433) do
-  create_table 'logs', force: :cascade do |t|
-    t.integer 'thing_id'
-    t.string 'action'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['thing_id'], name: 'index_logs_on_thing_id'
+ActiveRecord::Schema.define(version: 20180118183502) do
+
+  create_table "logs", force: :cascade do |t|
+    t.integer "thing_id"
+    t.string "action"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["thing_id"], name: "index_logs_on_thing_id"
   end
 
-  create_table 'things', force: :cascade do |t|
-    t.string 'name'
-    t.string 'status'
-    t.integer 'desired_value'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "things", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.integer "desired_value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index [nil], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
